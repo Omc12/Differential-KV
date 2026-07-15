22f522a9cd369c75b084b56c154736419b28072d
Om <omchimurkar10@gmail.com>
2026-07-15 23:54:47 +0530

    config(cuda): enable async_svd and torch_compile by default on CUDA for low and mid presets

--- ACTIVE_RUNTIME/native_core/config.py
+++ ACTIVE_RUNTIME/native_core/config.py
@@ -22,9 +22,9 @@ class DiffKVConfig:
             self.decode_cache_max_tokens = 0
             self.prefill_chunk_size = 256
             self.srl_threshold = 30
-            self.async_svd = False
+            self.async_svd = False if is_macos else True
             self.mps_watermark = 0.0
-            self.torch_compile = False
+            self.torch_compile = False if is_macos else True
             self.approximate_attn = True if is_macos else False
             self.srl_age_penalty = 0.01
             self.kv_quant = "q4_0"
@@ -51,7 +51,7 @@ class DiffKVConfig:
             self.srl_threshold = 50
             self.async_svd = False if is_macos else True  # Disable background async SVD on macOS for MPS stability
             self.mps_watermark = 0.0
-            self.torch_compile = False
+            self.torch_compile = False if is_macos else True
             self.approximate_attn = True if is_macos else False
             self.srl_age_penalty = 0.01
             self.kv_quant = "q8_0"
